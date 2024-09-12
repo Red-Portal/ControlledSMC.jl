49662e90712286fcffb59c392875af93bc5246b6
Kyurae Kim <kyrkim@seas.upenn.edu>
2024-09-11 22:26:36 -0700

    add PosteriorDB example with SMC-ULA

---
+++ scripts/stan_models_smc_ula.jl
@@ -0,0 +1,40 @@
+
+using Distributions
+using FillArrays
+using LinearAlgebra
+using Random, Random123
+
+using PosteriorDB
+using StanLogDensityProblems
+
+using ControlledSMC
+
+function main()
+    seed = (0x38bef07cf9cc549d, 0x49e2430080b3f797)
+    rng  = Philox4x(UInt64, seed, 8)
+    set_counter!(rng, 1)
+
+    if !isdir(".stan")
+        mkdir(".stan")
+    end
+    pdb  = PosteriorDB.database()
+    post = PosteriorDB.posterior(pdb, "dogs-dogs")
+    prob = StanProblem(post, ".stan/"; force=true)
+    d    = LogDensityProblems.dimension(prob)
+
+    n_iters  = 32
+    proposal = MvNormal(Zeros(d), I)
+    schedule = range(0, 1; length=n_iters).^3
+    path     = GeometricAnnealing(schedule, proposal, prob)
+
+    n_particles = 2^10
+
+    h0 = 0.0001
+    hT = 0.0001
+    Γ  = Eye(d)
+
+    sampler = SMCULA(Γ, h0, hT, TimeCorrectForwardKernel())
+    xs, _, info = ControlledSMC.sample(rng, sampler, path, n_particles, 0.5; show_progress=true)
+    Plots.scatter(xs[1,:], xs[2,:]) |> display
+    info
+end
