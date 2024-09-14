30caa56a9de9858628f6e1cdc87da6993817abb0
Kyurae Kim <kyrkim@seas.upenn.edu>
2024-09-13 21:24:34 -0700

    fix restructure tests

---
+++ test/unbiasedness.jl
@@ -0,0 +1,37 @@
+
+@testset "unbiasedness" begin
+    pvalue_threshold = 0.01
+    n_test_samples   = 128
+
+    # Problem Setup
+    d       = 5
+    μ       = Fill(10, d)
+    prob    = Dist(MvNormal(μ, I))
+    prob_ad = ADgradient(AutoReverseDiff(), prob)
+    Z_true  = 1.0
+
+    # Sampler Setup
+    n_iters     = 64
+    proposal    = MvNormal(Zeros(d), I)
+    schedule    = range(0, 1; length=n_iters)
+    path        = GeometricAnnealingPath(schedule, proposal, prob_ad)
+    n_particles = 256
+
+    @testset "$(name)" for (name, sampler) in [
+        (
+            "SMCULA + TimeCorrectForwardKernel",
+            SMCULA(0.5, 0.5, TimeCorrectForwardKernel(), Eye(d), path),
+        ),
+        ("SMCULA + ForwardKernel", SMCULA(0.5, 0.5, ForwardKernel(), Eye(d), path)),
+        ("SMCUHMC", SMCUHMC(1.0, 0.5, Eye(d))),
+    ]
+        ℓZ  = map(1:n_test_samples) do _
+            xs, _, _, stats = ControlledSMC.sample(sampler, path, n_particles, 0.5; show_progress=false)
+            last(stats).log_normalizer
+        end
+        Z   = exp.(ℓZ)
+        res = tTest1S(Z; refmean=Z_true, verbose=false)
+
+        @test res.p > pvalue_threshold
+    end
+end
