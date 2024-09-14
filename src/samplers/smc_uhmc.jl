37fa8881179a2fc5854c67b4838b8559121291b7
Kyurae Kim <kyrkim@seas.upenn.edu>
2024-09-13 19:17:52 -0700

    fix field names in SMC-UMHC

--- src/samplers/smc_uhmc.jl
+++ src/samplers/smc_uhmc.jl
@@ -1,8 +1,8 @@
 
 struct SMCUHMC{Stepsize<:Real,Mass<:AbstractMatrix} <: AbstractSMC
-    leapfrog_stepsize  :: Stepsize
-    diffusion_stepsize :: Stepsize
-    mass_matrix        :: Mass
+    stepsize    :: Stepsize
+    damping     :: Stepsize
+    mass_matrix :: Mass
 end
 
 function rand_initial_with_potential(
@@ -21,24 +21,24 @@ end
 function mutate_with_potential(
     rng::Random.AbstractRNG, sampler::SMCUHMC, t::Int, πt, πtm1, ztm1::AbstractMatrix
 )
-    (; leapfrog_stepsize, diffusion_stepsize, mass_matrix) = sampler
-
-    h, δ, M = diffusion_stepsize, leapfrog_stepsize, mass_matrix
+    (; stepsize, damping, mass_matrix) = sampler
+    ϵ, α, M = stepsize, damping, mass_matrix
+    sqrt1mα = sqrt(1 - α)
 
     d          = size(ztm1, 1) ÷ 2
     xtm1, vtm1 = ztm1[1:d, :], ztm1[(d + 1):end, :]
     v_dist     = MvNormal(Zeros(d), M)
 
-    vthalf = h * vtm1 + sqrt(1 - h^2) * unwhiten(M, randn(rng, size(vtm1)))
-    xt, vt = leapfrog_batch(πt, xtm1, vthalf, δ, M)
+    vthalf = sqrt1mα * vtm1 + sqrt(α) * unwhiten(M, randn(rng, size(vtm1)))
+    xt, vt = leapfrog_batch(πt, xtm1, vthalf, ϵ, M)
 
     ℓπt     = logdensity_batch(πt, xt)
     ℓπtm1   = logdensity_batch(πtm1, xtm1)
     ℓauxt   = logpdf.(Ref(v_dist), eachcol(vt))
     ℓauxtm1 = logpdf.(Ref(v_dist), eachcol(vtm1))
 
-    L  = MvNormal.(h * eachcol(vthalf), Ref((1 - h^2) * M))
-    K  = MvNormal.(h * eachcol(vtm1), Ref((1 - h^2) * M))
+    L  = MvNormal.(sqrt1mα * eachcol(vthalf), Ref(α * M))
+    K  = MvNormal.(sqrt1mα * eachcol(vtm1), Ref(α * M))
     ℓl = logpdf.(L, eachcol(vtm1))
     ℓk = logpdf.(K, eachcol(vthalf))
     ℓG = ℓπt + ℓauxt - ℓπtm1 - ℓauxtm1 + ℓl - ℓk
