3fd52fa03f118cc57e396db23db54217df43de5f
Kyurae Kim <kyrkim@seas.upenn.edu>
2024-09-19 21:25:27 -0700

    add missing file

---
+++ src/linalg/blas3.jl
@@ -0,0 +1,17 @@
+
+function transpose_square(
+    L::BlockLowerTriangular2by2{<:Diagonal},
+)
+    # [L11 L21] × [L11    ]
+    # [    L22]   [L21 L22]
+    #
+    # = [L11*L11 + L21*L21  L21*L22]
+    #   [          A22*A21  L22*L22]
+
+    (; L11, L21, L22) = L
+    BlockHermitian2by2(
+        L11*L11 + L21*L21,
+        L21*L22,
+        L22*L22,
+    )
+end
